ac46e31db29dac1efa09577fe08288c3ff25fb7e
semailk <sem.alik@mail.ru>
2026-07-15 22:15:48 +0400

    15 07 2026

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use function PHPSTORM_META\map;
+
+class WelcomeController extends Controller
+{
+    public function index(Request $request): View
+    {
+        $query = Product::query()->with([
+            'category',
+            'tags'
+        ]);
+
+        $categories = Category::query()->orderBy('name')->get();
+
+        list($column, $direction) = explode('-', $request->input('sort', 'created_at-desc'));
+
+        if ($request->input('search')) {
+            $query->where('name', 'like', '%' . $request->input('search') . '%')
+                ->orWhere('description', 'like', '%' . $request->input('search') . '%');
+        }
+
+        if ($request->input('price_from')) {
+            $query->where('price', '>=', $request->input('price_from'));
+        }
+        if ($request->input('price_to')) {
+            $query->where('price', '<=', $request->input('price_to'));
+        }
+        if ($request->input('category')) {
+            $query->where('category_id', $request->input('category'));
+        }
+
+        return view('welcome', [
+            'categories' => $categories,
+            'products' => $query
+                ->orderBy($column, $direction)
+                ->paginate(50)
+                ->withQueryString(),
+        ]);
+    }
+}
